61633aec265df3f08322f06b03237b180876a340
Carl Hörberg <carl.hoerberg@gmail.com>
2011-10-25 16:13:10 +0200

    Clean up in the PG backend

--- lib/meerkat/backend/pg.rb
+++ lib/meerkat/backend/pg.rb
@@ -1,5 +1,4 @@
 require 'pg'
-require 'date'
 
 module Meerkat
   module Backend
@@ -7,32 +6,12 @@ module Meerkat
       TABLENAME = 'meerkat_pubsub'.freeze
 
       def initialize(pg_uri = nil)
+        @pg_uri = pg_uri
         @subs = {}
         @pg = PGconn.connect pg_uri
-        @pg.exec 'SET client_min_messages = warning'
-        table = @pg.exec "SELECT true FROM pg_tables WHERE tablename = $1", [TABLENAME]
-        @pg.exec "CREATE TABLE #{TABLENAME} (topic varchar(1024), json text, timestamp timestamp default now())" if table.count == 0
-
         @last_check = @pg.exec('SELECT now() as now').first['now']
-
-        @sub_client = PGconn.connect pg_uri
-        @sub_client.exec "LISTEN #{TABLENAME}"
-        EM.next_tick {
-          EM.watch(@sub_client.socket, SubscribeClient, @sub_client, lambda {on_notify}) { |c| c.notify_readable = true }
-        }
-      end
-
-      def on_notify
-        @pg.async_exec "SELECT topic, json, timestamp FROM #{TABLENAME} WHERE timestamp > $1 ORDER BY timestamp ASC", [@last_check] do |rows|
-          rows.each do |row| 
-            @last_check = row['timestamp']
-            @subs.each do |topic, callbacks|
-              if topic == row['topic'] 
-                callbacks.each { |cb| cb.call row['json'] }
-              end
-            end
-          end
-        end
+        create_table
+        start_listener
       end
 
       def publish(route, json)
@@ -54,6 +33,35 @@ module Meerkat
 
       def unsubscribe(sid)
         @subs[sid[0]].delete sid[1]
+      end
+
+      private
+      def start_listener
+        @sub_client = PGconn.connect @pg_uri
+        @sub_client.exec "LISTEN #{TABLENAME}"
+        EM.next_tick {
+          EM.watch(@sub_client.socket, SubscribeClient, @sub_client, lambda {on_notify}) { |c| c.notify_readable = true }
+        }
+      end
+
+      def create_table
+        @pg.transaction do |conn|
+          table = conn.exec "SELECT true FROM pg_tables WHERE tablename = $1", [TABLENAME]
+          conn.exec "CREATE TABLE #{TABLENAME} (topic varchar(1024), json text, timestamp timestamp default now())" if table.count == 0
+        end
+      end
+
+      def on_notify
+        @pg.async_exec "SELECT topic, json, timestamp FROM #{TABLENAME} WHERE timestamp > $1 ORDER BY timestamp ASC", [@last_check] do |rows|
+          rows.each do |row| 
+            @last_check = row['timestamp']
+            @subs.each do |topic, callbacks|
+              if topic == row['topic'] 
+                callbacks.each { |cb| cb.call row['json'] }
+              end
+            end
+          end
+        end
       end
 
       module SubscribeClient
